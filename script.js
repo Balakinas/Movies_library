491367dfbb7a9fee2b17de0d21ae7cf1534406e1
Dmitriy <dmitriybalakinas@gmail.com>
2023-06-25 22:31:05 +0300

    creating variables in script file for Movies Library

---
+++ script.js
@@ -0,0 +1,22 @@
+// const numberOfFilms = prompt("Hom much movies did you watch?", "");
+
+// personalMovieDB = {
+//   "count": +numberOfFilms,
+//   "movies": {},
+//   "actors": {},
+//   "genres": [],
+//   "privat": false
+// };
+
+// firstMovie = prompt("first movie?", "");
+// firstMovieRate = prompt("how would you rate it?", "");
+// secondtMovie = prompt("second movie?", "");
+// secondMovieRate = prompt("how would you rate it?", "");
+
+// personalMovieDB.movies[firstMovie] = firstMovieRate;
+// personalMovieDB.movies[secondtMovie] = secondMovieRate;
+
+
+// console.log(personalMovieDB);
+
+
